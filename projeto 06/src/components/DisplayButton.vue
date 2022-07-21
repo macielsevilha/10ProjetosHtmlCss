937835fdbe456c1845008f6342315fc8266f43b4
MacielSevilha <dev.macielsevilha@gmail.com>
2022-07-20 21:51:49 -0300

    fazendo o check

--- projeto 06/src/components/DisplayButton.vue
+++ projeto 06/src/components/DisplayButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex justify-content-center">
-    <b-button class=""  pill size="lg" block variant="primary mx-5 py-2">Sign up
-      <b-icon  class="float-right mt-1 font-weight-bold" icon="arrow-right" scale="1" variant="white"></b-icon>
+    <b-button class=""  pill size="lg" block variant="primary mx-4 py-[10px]">Sign up
+      <b-icon class="float-right mt-1 font-weight-bold" icon="arrow-right" scale="1" variant="white"></b-icon>
     </b-button>
   </div>
 </template>
--- projeto 06/src/components/DisplayLogin.vue
+++ projeto 06/src/components/DisplayLogin.vue
@@ -1,40 +1,47 @@
 <template>
- <div class="login">
-  <h1>Create account</h1>
-   <form action="">
+  <div class="login">
+    <h1>Create account</h1>
+    <form action="">
       <label for="">Already have an account? <a href="">Sign in</a></label>
-      <input type="text" name="" placeholder="name">
-      <input type="text" name="" placeholder="First name">
-      <input type="text" name="" placeholder="Last name">
-      <input type="text" name="" placeholder="E-mail">
-      <div class="button"> 
+      <input type="text" name="" placeholder="name" />
+      <input type="text" name="" placeholder="First name" />
+      <input type="text" name="" placeholder="Last name" />
+      <input type="text" name="" placeholder="E-mail" />
+      <div class="button">
         <display-button-vue />
       </div>
-      <div class="ckeck">
-        <input type="checkbox" name="" id="">
-        <label for="">I have read and agree to the Terms of Service</label>
+      <div class="my-4 mx-4">
+        <b-form-checkbox v-model="status">
+          I have read and agree to the
+          <b-link href="#foo" disabled> Terms of Service </b-link>
+          <div>
+            State: <strong>{{ status }}</strong>
+          </div>
+        </b-form-checkbox>
       </div>
-      <b-icon-arrow-up></b-icon-arrow-up>
-   </form>
- </div>
+    </form>
+  </div>
 </template>
 
 <script>
-import DisplayButtonVue from './DisplayButton.vue'
+import DisplayButtonVue from "./DisplayButton.vue";
 export default {
-  name: 'DisplayLogin',
+  name: "DisplayLogin",
   components: {
     DisplayButtonVue,
-  
-  }
-}
+  },
+  data() {
+    return {
+      status: false,
+    };
+  },
+};
 </script>
 
 <style>
-  .login {
-    background: rgb(133, 131, 131);
-    
-    width: 500px;
-   
-  }
+.login {
+  background: rgb(133, 131, 131);
+
+  width: 500px;
+}
 </style>
